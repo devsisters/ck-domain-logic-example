ee6cd6ddaec92ce1864e3ce310218db332fe8ee6
Jihoon Kim <jihoon.kim@devsisters.com>
2023-02-16 22:10:44 +0900

    Move values

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -45,36 +45,36 @@ import example.logic.Programs._
 //}
 
 object Main {
-  val item1DataId     = DataId("item1")
-  val item2DataId     = DataId("item2")
-  val shopSlot1DataId = DataId("shopSlot1")
-  val shopSlot2DataId = DataId("shopSlot2")
-
-  val data = Data(
-    Map(
-      item1DataId -> ItemData(item1DataId, "Item 1"),
-      item2DataId -> ItemData(item2DataId, "Item 2")
-    ),
-    ShopData(
-      Map(
-        shopSlot1DataId -> ShopSlotData(shopSlot1DataId, 300, item1DataId, 20),
-        shopSlot2DataId -> ShopSlotData(shopSlot2DataId, 5000, item2DataId, 1)
-      )
-    )
-  )
-
-  val initialState = State(
-    1000,
-    Map.empty,
-    Shop(
-      Map(
-        shopSlot1DataId -> ShopSlot(shopSlot1DataId, 0),
-        shopSlot2DataId -> ShopSlot(shopSlot2DataId, 0)
-      )
-    )
-  )
-
   def main(args: Array[String]): Unit = {
+    val item1DataId     = DataId("item1")
+    val item2DataId     = DataId("item2")
+    val shopSlot1DataId = DataId("shopSlot1")
+    val shopSlot2DataId = DataId("shopSlot2")
+
+    val data = Data(
+      Map(
+        item1DataId -> ItemData(item1DataId, "Item 1"),
+        item2DataId -> ItemData(item2DataId, "Item 2")
+      ),
+      ShopData(
+        Map(
+          shopSlot1DataId -> ShopSlotData(shopSlot1DataId, 300, item1DataId, 20),
+          shopSlot2DataId -> ShopSlotData(shopSlot2DataId, 5000, item2DataId, 1)
+        )
+      )
+    )
+
+    val initialState = State(
+      1000,
+      Map.empty,
+      Shop(
+        Map(
+          shopSlot1DataId -> ShopSlot(shopSlot1DataId, 0),
+          shopSlot2DataId -> ShopSlot(shopSlot2DataId, 0)
+        )
+      )
+    )
+
     val scenario =
       for {
         () <- purchaseShopSlot(shopSlot1DataId)
